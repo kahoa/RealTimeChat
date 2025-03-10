8bd2a2faaebf3d4888870b7d56a3dfeb127403b4
Kaho.A <papagei0303@gmail.com>
2025-03-10 19:05:53 +0100

    Revert "fix: Try to fix InvalidClientTokenId"
    This reverts commit a87ad123f988bcc8804617ec0012f734f3e4a9db.

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 provider "aws" {
+<<<<<<< HEAD
   region  = "eu-central-1"
   profile = "default"
+=======
+  region = "eu-central-1"
+>>>>>>> parent of a87ad12 (fix: Try to fix InvalidClientTokenId)
 }
 resource "aws_instance" "example" {
   ami           = "ami-0b74f796d330ab49c"
