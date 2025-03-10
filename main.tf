4bbc89ff1dd86b7189ad59eeff5235da5b867e5e
Kaho.A <papagei0303@gmail.com>
2025-03-10 18:52:19 +0100

    refactor: Fix formatting

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 resource "aws_instance" "example" {
-    ami = "ami-0b74f796d330ab49c"
-    instance_type = "t2.micro"
+  ami           = "ami-0b74f796d330ab49c"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "MyFirstEC2Instance"
-        }
-    }
+  tags = {
+    Name = "MyFirstEC2Instance"
+  }
+}
 
