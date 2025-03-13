3867cf3e6c26324ee400baca8b65d7b27cdd3fde
Kaho.A <papagei0303@gmail.com>
2025-03-13 16:21:12 +0100

    fix: Use the DNS instead of IP for CORS

--- infrastructure/Terraform/terraform-output-to-json.sh
+++ infrastructure/Terraform/terraform-output-to-json.sh
@@ -6,7 +6,7 @@ yes | cp terraform_outputs.json ../Ansible/
 echo 'Creating Ansible inventory file'
 echo "[webserver]" > ../Ansible/inventory.ini
 echo "$(jq -r .ec2_public_dns.value < terraform_outputs.json) ansible_user=ec2-user ansible_ssh_private_key_file=../Terraform/terraform-key.pem" >> ../Ansible/inventory.ini
-echo 'Extracting EC2 Public IP and saving to .env'
-echo "VITE_WS_SERVER=$(jq -r .ec2_public_ip.value < terraform_outputs.json)" > ../../frontend/.env
+echo 'Extracting EC2 Public DNS and saving to .env'
+echo "VITE_WS_SERVER=$(jq -r .ec2_public_dns.value < terraform_outputs.json)" > ../../frontend/.env
 
 echo 'Done! The WebSocket server IP is now set in frontend/.env'
