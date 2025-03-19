67d1c608d93f6a36ec7d5449911f456b4fae105c
Kaho.A <papagei0303@gmail.com>
2025-03-19 22:24:58 +0100

    chore: Create initial Eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // Disable the rule that requires React to be in scope
+    },
+  },
+];
